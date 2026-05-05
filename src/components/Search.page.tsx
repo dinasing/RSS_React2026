bc693e4baba340310a079010e9856c6d03673f77
Dina Singatulina <dina.sing1212@gmail.com>
2026-05-05 11:17:12 +0300

    fix: center loader message

--- src/components/Search.page.tsx
+++ src/components/Search.page.tsx
@@ -102,7 +102,7 @@ class SearchPage extends Component {
         className="absolute top-0 left-0
         items-center min-h-dvh min-w-dvw backdrop-blur-sm transition-opacity duration-300"
       >
-        <div className="text-2xl italic text-white md:p-96 p-[40%]">
+        <div className="text-2xl italic text-white md:p-96 p-[40%] text-center">
           So many books, so little time...
         </div>
       </div>
