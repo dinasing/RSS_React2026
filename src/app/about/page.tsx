d9f3ee7ca58f53fe2e0cf0bb241ea1fc8de0a2fd
dinasing <dina.sing1212@gmail.com>
2026-06-22 21:47:16 +0300

    refactor: simplify About page by integrating AboutPage component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,41 +1,5 @@
-import Link from 'next/link';
-import GoHome from '../../components/GoHome/GoHome.component';
+import AboutPage from '../../views/About/About.page';
 
-const About = () => (
-  <>
-    <div className="flex flex-col gap-6">
-      <h1 className="text-3xl font-bold">About page</h1>
-      <p className="text-lg">
-        This is a simple app that allows you to search for books using the{' '}
-        <Link
-          className="text-blue-500 hover:text-blue-600 bg-white px-2 py-1 rounded-md"
-          href="https://openlibrary.org/"
-        >
-          Open Library
-        </Link>{' '}
-        API
-      </p>
-      <p className="text-md">
-        Author: dinasing{' '}
-        <Link
-          className="text-blue-500 hover:text-blue-600 bg-white px-2 py-1 rounded-md"
-          href="https://github.com/dinasing"
-        >
-          GitHub
-        </Link>
-      </p>
-      <p className="text-lg">
-        This app was implemented as part of the{' '}
-        <Link
-          className="text-blue-500 hover:text-blue-600 bg-white px-2 py-1 rounded-md"
-          href="https://rs.school/courses/reactjs"
-        >
-          RSS React 2026 course
-        </Link>
-      </p>
-      <GoHome />
-    </div>
-  </>
-);
-
-export default About;
+export default function About() {
+  return <AboutPage />;
+}
