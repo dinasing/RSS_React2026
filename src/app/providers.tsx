e38d20b188c166078e15a9d27853e2014a9e149e
dinasing <dina.sing1212@gmail.com>
2026-06-22 21:46:59 +0300

    feat: integrate next-intl for internationalization support and add English and Russian translations

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,38 @@
 'use client';
 
+import { NextIntlClientProvider } from 'next-intl';
 import { Provider } from 'react-redux';
+import {
+  LocaleProvider,
+  useLocaleContext,
+} from '../context/Locale/Locale.context';
 import { ThemeProvider } from '../context/Theme/Theme.context';
+import LocaleSync from '../components/LocaleSync/LocaleSync.component';
 import { store } from '../store/store';
 
 type ProvidersProps = {
   children: React.ReactNode;
 };
 
+function IntlProvider({ children }: ProvidersProps) {
+  const { locale, messages } = useLocaleContext();
+
+  return (
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <LocaleSync />
+      {children}
+    </NextIntlClientProvider>
+  );
+}
+
 export function Providers({ children }: ProvidersProps) {
   return (
-    <ThemeProvider>
-      <Provider store={store}>{children}</Provider>
-    </ThemeProvider>
+    <LocaleProvider>
+      <IntlProvider>
+        <ThemeProvider>
+          <Provider store={store}>{children}</Provider>
+        </ThemeProvider>
+      </IntlProvider>
+    </LocaleProvider>
   );
 }
